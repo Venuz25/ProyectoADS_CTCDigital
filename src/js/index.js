eb9f358e7a8338ebd0df15df60a1ec1415b140a0
Venuz25 <areli.gbadillo@outlook.com>
2025-04-22 14:35:49 -0600

    :)

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-const images = document.querySelectorAll('.carrusel-image');
+const items = document.querySelectorAll('.carrusel-item');
 const dots = document.querySelectorAll('.dot');
 const leftArrow = document.querySelector('.arrow.left');
 const rightArrow = document.querySelector('.arrow.right');
@@ -6,20 +6,20 @@ const rightArrow = document.querySelector('.arrow.right');
 let current = 0;
 
 function showSlide(index) {
-  images.forEach(img => img.classList.remove('active'));
+  items.forEach(item => item.classList.remove('active'));
   dots.forEach(dot => dot.classList.remove('active'));
-  images[index].classList.add('active');
+  items[index].classList.add('active');
   dots[index].classList.add('active');
   current = index;
 }
 
 leftArrow.addEventListener('click', () => {
-  const newIndex = (current - 1 + images.length) % images.length;
+  const newIndex = (current - 1 + items.length) % items.length;
   showSlide(newIndex);
 });
 
 rightArrow.addEventListener('click', () => {
-  const newIndex = (current + 1) % images.length;
+  const newIndex = (current + 1) % items.length;
   showSlide(newIndex);
 });
 
